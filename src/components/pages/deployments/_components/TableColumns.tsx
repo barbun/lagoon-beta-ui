caec22b82810a8f74fb904e65fe38d0a46aa7480
cgoodwin90 <chris_2190@hotmail.com>
2026-02-02 10:37:59 +1100

    Updated styled Link to use Badge component for consistency + minor prettier formatting

--- src/components/pages/deployments/_components/TableColumns.tsx
+++ src/components/pages/deployments/_components/TableColumns.tsx
@@ -14,6 +14,7 @@ import utc from 'dayjs/plugin/utc';
 import { dateRangeFilter } from 'utils/tableDateRangeFilter';
 import { Check, Loader2, X } from 'lucide-react';
 import { getBadgeVariant } from 'utils/setBadgeStatus';
+import { Link as LinkIcon } from 'lucide-react';
 
 dayjs.extend(isBetween);
 dayjs.extend(duration);
@@ -57,7 +58,7 @@ const getDeploymentTableColumns = (basePath: string) =>
               {(status == "failed" || status == "cancelled") && <X color="red" size="16" />}
               <Badge variant={getBadgeVariant(status, buildStep)}>
                 {capitalize(status)}
-                {!['complete', 'cancelled', 'failed'].includes(status) && <Loader2 className="h-4 w-4 animate-spin t"/>}
+                {!['complete', 'cancelled', 'failed'].includes(status) && <Loader2 className="h-4 w-4 animate-spin t" />}
               </Badge>
             </div>
 
@@ -93,13 +94,16 @@ const getDeploymentTableColumns = (basePath: string) =>
       cell: ({ row }) => {
         const { bulkId, name } = row.original;
         return (
-          <section className="flex justify-between">
+          <section className={`flex items-center ${bulkId ? 'justify-between pr-12' : ''}`}>
             <Link className="hover:text-blue-800 transition-colors" href={`${basePath}/${name}`}>
               {name}
             </Link>
             {bulkId ? (
-              <Link className="px-2 py-1 bg-sky-500 text-white rounded text-sm hover:bg-sky-600 transition-colors whitespace-nowrap mr-10" href={`/bulkdeployment/${bulkId}`}>
-                BULK
+              <Link className="py-1 px-2" href={`/bulkdeployment/${bulkId}`}>
+                <Badge variant="info" className="py-1 px-2 rounded-sm">
+                  BULK
+                  <LinkIcon className="h-4 w-4" />
+                </Badge>
               </Link>
             ) : null}
           </section>
